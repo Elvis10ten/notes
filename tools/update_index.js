22aaa4697a6624eb936c5047cffbcfe2f51b1164
Elvis Chidera <elvis.chidera@gmail.com>
2024-11-23 14:26:26 +0100

    changes

--- tools/update_index.js
+++ tools/update_index.js
@@ -38,7 +38,7 @@ for (const year in indexMap) {
 
     notesMetadata.forEach(noteMetadata => {
         const path = sep + relative(projectDirName, getSrcPath(noteMetadata.noteFileName));
-        yearSection += `\n- ‣ [${noteMetadata.title}](${path})`;
+        yearSection += `\n- [${noteMetadata.title}](${path})`;
     });
 
     indexSections.push(yearSection);
